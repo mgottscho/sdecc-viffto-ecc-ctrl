d0d257382560f3c530328ec7aedf692dd7c4f4bf
Mark Gottscho <mgottscho@ucla.edu>
2016-07-19 14:00:30 -0700

    Added quick script to plot the heuristic recovery details for a single benchmark using instruction-based recovery for RISC-V

---
+++ heuristic_recovery_plots.m
@@ -0,0 +1,20 @@
+x = mean(results_valid_messages);
+y = mean(success);
+
+
+figure;
+scatter(x(opcodes_affected),y(opcodes_affected),'r');
+hold on;
+scatter(x(rd_affected),y(rd_affected),'b');
+scatter(x(funct3_affected),y(funct3_affected),'g');
+scatter(x(rs1_affected),y(rs1_affected),'k');
+scatter(x(rs2_affected),y(rs2_affected),'y');
+scatter(x(funct7_affected),y(funct7_affected),'c');
+scatter(x(parity_affected),y(parity_affected),'k.');
+xlim([0 20]);
+ylim([0 1]);
+
+legend('opcode','rd','funct3','rs1','rs2','funct7','parity');
+xlabel('Average Number of Valid (Filtered) Candidate Messages');
+ylabel('Average Rate of Recovery');
+title(['Rate of Heuristic Recovery for ' code_type ' (39,32) SECDED -- ' benchmark ' -- Filter-rank-filter-rank']);
