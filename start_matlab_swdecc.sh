81781dc2328a6e971422698e9030aa8909e16c1a
Mark Gottscho <mgottscho@ucla.edu>
2016-07-20 18:30:12 -0700

    Updates to compile matlab code on Hoffman, use updated Riscv Decoder compiled for Hoffman, etc

--- start_matlab_swdecc.sh
+++ start_matlab_swdecc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
-# Use this script to start MATLAB on MWG-Desktop-UbuntuVM. This is needed because matlab will not find the necessary shared libraries for using MyRiscvDecode.mexa64.
+# Use this script to start MATLAB. This is needed because matlab will not find the necessary shared libraries for using MyRiscvDecode.mexa64.
 
-LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 matlab
+#LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 matlab # For MWG-Desktop-UbuntuVM
+LD_PRELOAD=$GCC5/lib64/libstdc++.so.6 matlab # For Hoffman2 -- not yet tested
