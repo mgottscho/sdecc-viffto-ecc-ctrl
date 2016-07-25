0907e7a2c8ed9c422e2a2a9268d607568ce2b441
Mark Gottscho <mgottscho@ucla.edu>
2016-07-25 16:24:31 -0700

    More fixes to count_longest_run.m

--- count_longest_run.m
+++ count_longest_run.m
@@ -1,16 +1,14 @@
 function [longestRun] = count_longest_run(bin)
 
-bin
-
 longestRun = size(bin,2); % Init to max
-zeros_start = find(diff(bin)==-1)+1 % multiple positions
-zeros_end = find(diff(bin)==1)+1 % multiple positions
-zeros_longestRun = max(zeros_end-zeros_start)
+zeros_start = find(diff(['1' bin])==-1); % multiple positions
+zeros_end = find(diff([bin '1'])==1); % multiple positions
+zeros_longestRun = max(zeros_end-zeros_start)+1;
 
-ones_start = find(diff(['0' bin])==1) % multiple positions
-ones_end = find(diff([bin '0'])==-1)+1 % multiple positions
-ones_longestRun = max(ones_end-ones_start)
+ones_start = find(diff(['0' bin])==1); % multiple positions
+ones_end = find(diff([bin '0'])==-1); % multiple positions
+ones_longestRun = max(ones_end-ones_start)+1;
 
-longestRun = max(zeros_longestRun,ones_longestRun)
+longestRun = max(zeros_longestRun,ones_longestRun);
 
 end
