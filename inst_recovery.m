fde3c519ad3ce92fbeda17dca0abb93c14e9c422
Mark Gottscho <mgottscho@ucla.edu>
2016-07-29 17:20:46 -0700

    Wrote a new function inst_recovery() that acts like a standalone SWD-ECC implementation that is seperate from the surrounding experimental framework. The intent is to compile it and link it into my Spike simulations or to use it in the MATLAB offline analysis with the same source code.

--- inst_recovery.m
+++ inst_recovery.m
@@ -1,4 +1,4 @@
-function [message, could_crash] = inst_recovery(architecture, n, k, received_string, code_type, policy, tiebreak_policy, mnemonic_hotness_filename, rd_hotness_filename)
+function [original_codeword, received_string, recovered_message, suggest_to_crash] = inst_recovery(architecture, n, k, original_message, error_pattern, code_type, policy, tiebreak_policy, mnemonic_hotness_filename, rd_hotness_filename)
 % This function attempts to heuristically recover from a DUE affecting a single received string.
 % The message is assumed to be an instruction of the given architecture.
 % To compute candidate codewords, we flip a single bit one at a time and decode using specified SECDED decoder..
@@ -9,7 +9,8 @@ function [message, could_crash] = inst_recovery(architecture, n, k, received_str
 %   architecture --     String: '[mips|alpha|rv64g]'
 %   n --                String: '[39|72]'
 %   k --                String: '[32|64]'
-%   received_string --  Binary String
+%   original_message -- Binary String of length k bits/chars
+%   error_pattern --    Binary String of length n bits/chars
 %   code_type --        String: '[hsiao|davydov1991]'
 %   policy --           String: '[filter-rank|filter-rank-filter-rank]'
 %   tiebreak_policy --   String: '[pick_first|pick_last|pick_random]'
@@ -17,8 +18,10 @@ function [message, could_crash] = inst_recovery(architecture, n, k, received_str
 %   rd_hotness_filename -- String: full path to CSV file containing the relative frequency of each destination register address to use for ranking
 %
 % Returns:
-%   message -- k-bit message that is the recovery target.
-%   could_crash -- 0 if we are confident in recovery, 1 if we may prefer to crash
+%   original_codeword -- n-bit encoded version of original_message
+%   received_string -- n-bit string that is corrupted by the bit flips specified by error_pattern
+%   recovered_message -- k-bit message that corresponds to our target for heuristic recovery
+%   suggest_to_crash -- 0 if we are confident in recovery, 1 if we recommend crashing out instead
 %
 % Author: Mark Gottscho
 % Email: mgottscho@ucla.edu
@@ -26,14 +29,15 @@ function [message, could_crash] = inst_recovery(architecture, n, k, received_str
 architecture
 n = str2num(n)
 k = str2num(k)
-received_string
+original_message
+error_pattern
 code_type
 policy
 tiebreak_policy
 mnemonic_hotness_filename
 rd_hotness_filename
 
-r = n-k;
+suggest_to_crash = 0; % Init
 
 %% Get our ECC encoder and decoder matrices
 display('Getting ECC encoder and decoder matrices...');
@@ -41,18 +45,34 @@ display('Getting ECC encoder and decoder matrices...');
 
 %% Read mnemonic and rd distributions from files now -- TODO
 display('Importing static instruction distribution...');
-instruction_mneumonic_hotness = containers.Map(); % Init
+instruction_mnemonic_hotness = containers.Map(); % Init
 instruction_rd_hotness = containers.Map(); % Init
 
-%% Attempt to decode the corrupted codeword, check that num_error_bits is 2
-[decoded_message, num_error_bits] = secded_decoder(received_string, H, code_type);
-        
-% Sanity check
-if num_error_bits ~= 2
-   display(['OOPS! Number of bits in error in received string was ' num_error_bits '. Continuing anyway.']);
+%% Encode the original message, then corrupt the codeword with the provided error pattern
+display('Getting the original codeword and generating the received (corrupted) string...');
+original_codeword = secded_encoder(original_message,G)
+received_string = dec2bin(bitxor(bin2dec(original_codeword), bin2dec(error_pattern)), n)
+
+%% Attempt to recover the original message. This could actually succeed depending on the code used and how many bits are in the error pattern.
+display('Attempting to decode the received string...');
+[recovered_message, num_error_bits] = secded_decoder(received_string, H, code_type);
+display(['ECC decoder determined that there are ' num2str(num_error_bits) ' bits in error. The input error pattern had ' num2str(sum(error_pattern=='1')) ' bits flipped.']);
+
+%% If the ECC decoder had no error or correctable error, we are done
+if num_error_bits == 0 || num_error_bits == 1 
+    display('No error or correctable error. We are done, no need for heuristic recovery.');
+    if sum(error_pattern=='1') ~= num_error_bits
+        display('NOTE: This is a MIS-CORRECTION by the ECC decoder itself.');
+    end
+    return;
 end
-        
-%% Flip 1 bit at a time on the received codeword, and attempt decoding on each. We should find several bit positions that decode successfully with just a single-bit error.
+
+%% If we got to this point, we have to recover from a DUE. 
+display('Attempting heuristic recovery...');
+
+%% Flip 1 bit at a time on the corrupted codeword, and attempt decoding on each. We should find several bit positions that decode successfully with just a single-bit error.
+display('Computing candidate codewords...');
+recovered_message = repmat('X',1,k); % Re-init
 x = 1;
 candidate_correct_messages = repmat('X',n,k); % Pre-allocate for worst-case capacity. X is placeholder
 for pos=1:n
@@ -72,107 +92,93 @@ for pos=1:n
    end
 end
 
-%% Uniquify the candidate messages
+% Uniquify the candidate messages
 if x > 1
     candidate_correct_messages = candidate_correct_messages(1:x-1, :);
     candidate_correct_messages = unique(candidate_correct_messages,'rows');
 else
-    display(['Something went wrong! x = ' num2str(x)]);
+    display('FATAL! Something went wrong computing candidate-correct messages!');
+    return;
 end
 
 
-%% Now check each of the candidate codewords to see which are valid instructions :)
+%% RECOVERY STEP 1: FILTER. Check each of the candidate codewords to see which are valid instructions
+display('RECOVERY STEP 1: FILTER. Filtering candidate codewords for instruction legality...');
 num_candidate_messages = size(candidate_correct_messages,1);
 num_valid_messages = 0;
 candidate_valid_messages = repmat('0',1,k); % Init
-valid_messages_mneumonic = cell(1,1);
+valid_messages_mnemonic = cell(1,1);
 valid_messages_rd = cell(1,1);
 for x=1:num_candidate_messages
-    %% Convert message to hex string representation
+    % Convert message to hex string representation
     message = candidate_correct_messages(x,:);
     message_hex = dec2hex(bin2dec(message),8);
     
-    %% Test the candidate message to see if it is a valid instruction and extract disassembly of the message hex
-    %if strcmp(computer(), 'PCWIN64') == 1 % Windows version of the decode program
-    %    status = dos([architecture 'decode ' message_hex ' >tmp_disassembly_' architecture '_' benchmark '_' num2str(i) '.txt']);
-    %elseif strcmp(computer(), 'MACI64') == 1 % Mac version of the decode program
-    %    status = unix(['./' architecture 'decode-mac ' message_hex ' >tmp_disassembly_' architecture '_' benchmark '_' num2str(i) '.txt']); % Mac version of the decode program
-    %elseif strcmp(computer(), 'GLNXA64') == 1 % Linux version of the decode program
-        %status = unix(['./' architecture 'decode-linux ' message_hex ' >tmp_disassembly_' architecture '_' benchmark '_' num2str(i) '.txt']); % Linux version of the decode program
-        if strcmp(architecture,'mips') == 1
-            [status, decoderOutput] = MyMipsDecoder(message_hex);
-        elseif strcmp(architecture,'alpha') == 1
-            [status, decoderOutput] = MyAlphaDecoder(message_hex);
-        elseif strcmp(architecture,'rv64g') == 1
-            [status, decoderOutput] = MyRv64gDecoder(message_hex);
-        else
-            display('ERROR! Supported ISAs are mips, alpha, and rv64g');
-            status = -1;
-            decoderOutput = '';
-    %        exit(1);
-        end 
-    %else % Error
-    %    display('Non-supported operating system detected!');
-    %    status = 1;
-    %end
+    % Test the candidate message to see if it is a valid instruction and extract disassembly of the message hex
+    if strcmp(architecture,'rv64g') == 1
+        [status, decoderOutput] = MyRv64gDecoder(message_hex);
+    else
+        display('FATAL! Supported ISA is rv64g.'); % Need to bring mips/alpha decoders up to date if we want to use them here
+        return;
+    end 
     
-    if status == 0 % It is valid!
+    if status == 0 % It is valid! Track it. Otherwise, ignore.
        num_valid_messages = num_valid_messages+1;
        candidate_valid_messages(num_valid_messages,:) = message;
        
-       % Read disassembly of instruction from file
-       %tmpfid = fopen(['tmp_disassembly_' architecture '_' benchmark '_' num2str(i) '.txt']);
-       %tmp_file_contents = textscan(tmpfid, '%s', 'Delimiter', ':');
+       % Read disassembly of instruction from string spit back by the instruction decoder
        output_contents = textscan(decoderOutput, '%s', 'Delimiter', ':');
-       %fclose(tmpfid);
-
-       %tmp_file_contents = tmp_file_contents{1};
        output_contents = output_contents{1};
-       %tmp_file_contents = reshape(tmp_file_contents, 2, size(tmp_file_contents,1)/2)';
        output_contents = reshape(output_contents, 2, size(output_contents,1)/2)';
 
        % Store disassembly in the list
-       mneumonic = output_contents{4,2};
+       mnemonic = output_contents{4,2};
        rd = output_contents{6,2};
-       valid_messages_mneumonic{num_valid_messages,1} = mneumonic;
+       valid_messages_mnemonic{num_valid_messages,1} = mnemonic;
        valid_messages_rd{num_valid_messages,1} = rd;
     end
 end
 
-%% Choose decode target
-highest_rel_freq_mneumonic = 0;
-target_mneumonic = '';
+%% RECOVERY STEP 2: RANK. Sort valid messages in order of their relative frequency as determined by the input file that we read.
+display('RECOVERY STEP 2: RANK. Sort valid messages in order of their relative frequency of mnemonic as determined by input tables...');
+highest_rel_freq_mnemonic = 0;
+target_mnemonic = '';
 for x=1:num_valid_messages
-    mneumonic = valid_messages_mneumonic{x,1};
-    if instruction_mneumonic_hotness.isKey(mneumonic)
-        rel_freq_mneumonic = instruction_mneumonic_hotness(mneumonic);
+    mnemonic = valid_messages_mnemonic{x,1};
+    if instruction_mnemonic_hotness.isKey(mnemonic)
+        rel_freq_mnemonic = instruction_mnemonic_hotness(mnemonic);
     else % This could happen legally
-        rel_freq_mneumonic = 0;
+        rel_freq_mnemonic = 0;
     end
     
     % Find highest frequency mneumonic
-    if rel_freq_mneumonic >= highest_rel_freq_mneumonic
-       highest_rel_freq_mneumonic = rel_freq_mneumonic;
-       target_mneumonic = mneumonic;
+    if rel_freq_mnemonic >= highest_rel_freq_mnemonic
+       highest_rel_freq_mnemonic = rel_freq_mnemonic;
+       target_mnemonic = mnemonic;
     end
 end
 
 % Find indices matching highest frequency mneumonic
-mneumonic_inst_indices = zeros(1,1);
+mnemonic_inst_indices = zeros(1,1);
 y=1;
 for x=1:num_valid_messages
-    mneumonic = valid_messages_mneumonic{x,1};
-    if strcmp(mneumonic,target_mneumonic) == 1
-        mneumonic_inst_indices(y,1) = x;
+    mnemonic = valid_messages_mnemonic{x,1};
+    if strcmp(mnemonic,target_mnemonic) == 1
+        mnemonic_inst_indices(y,1) = x;
         y = y+1;
     end
 end
 
+target_inst_indices = mnemonic_inst_indices; % By default, targets are finished here unless we do filter-rank-filter-rank policy.
+
 if strcmp(policy,'filter-rank-filter-rank') == 1 % match
+    %% RECOVERY STEP 3 (OPTIONAL): FILTER. Select only the valid messages with the most common mnemonic.
+    display('RECOVERY STEP 3 (OPTIONAL): FILTER. Select only the valid messages with the most common mnemonic...');
+    target_inst_indices = zeros(1,1);
     highest_rel_freq_rd = 0;
     target_rd = '';
-    for y=1:size(mneumonic_inst_indices,1)
-       rd = valid_messages_rd{mneumonic_inst_indices(y,1),1};
+    for y=1:size(mnemonic_inst_indices,1)
+       rd = valid_messages_rd{mnemonic_inst_indices(y,1),1};
 
        if instruction_rd_hotness.isKey(rd)
            rel_freq_rd = instruction_rd_hotness(rd);
@@ -187,25 +193,20 @@ if strcmp(policy,'filter-rank-filter-rank') == 1 % match
        end
     end
 
-    % Find indices matching both highest frequency mneumonic and highest frequency rd
-    target_inst_indices = zeros(1,1);
+    %% RECOVERY STEP 4 (OPTIONAL): RANK. Rank the set of valid messages with the most common mnemonic followed by the most common destination register address.
+    display('RECOVERY STEP 3 (OPTIONAL): FILTER. Rank by the most common destination register address...');
     z=1;
-    for y=1:size(mneumonic_inst_indices,1)
-       rd = valid_messages_rd{mneumonic_inst_indices(y,1),1};
+    for y=1:size(mnemonic_inst_indices,1)
+       rd = valid_messages_rd{mnemonic_inst_indices(y,1),1};
        if strcmp(rd,target_rd) == 1
-           target_inst_indices(z,1) = mneumonic_inst_indices(y,1);
+           target_inst_indices(z,1) = mnemonic_inst_indices(y,1);
            z = z+1;
        end
     end
     
     if target_inst_indices(1) == 0 % This is OK when rd is not used anywhere in the checked candidates
-        target_inst_indices = mneumonic_inst_indices;
+        target_inst_indices = mnemonic_inst_indices;
     end
-elseif strcmp(policy, 'filter-rank') == 1 % match
-    target_inst_indices = mneumonic_inst_indices; 
-else % Error
-    print(['Invalid recovery policy: ' policy]);
-    target_inst_indices = -1;
 end
 
 % REVELATION 7/24/2016: deterministically choosing the target instruction index has a HUGE effect on recovery rate!!!!!!! We thought this should never happen.
@@ -213,14 +214,15 @@ end
 % In the more recent work, we always chose the *first* of valid messages as the target. This corresponds to a candidate with trial flips towards the MSB in a codeword.
 % The latter strategy performs MUCH better: 60% vs 45% for bzip2 on filter-rank policy, typically. WHY?? We thought they should be equivalent to a random choice...
 % FIXME and UNDERSTAND
-crash = 0;
+
+%% Choose recovery target and decide if crashing is recommended
 if target_inst_indices(1) == 0 % sanity check
-    display('Error! No valid target instruction for recovery found.');
+    display('FATAL! No valid target instruction for recovery found.');
     target_inst_index = -2;
 elseif size(target_inst_indices,1) == 1 % have one recovery target
     target_inst_index = target_inst_indices(1); 
 else % multiple recovery targets: allowed crash.
-    crash = 1;
+    suggest_to_crash = 1;
     if strcmp(tiebreak_policy, 'pick_first') == 1
         target_inst_index = target_inst_indices(1);
     elseif strcmp(tiebreak_policy, 'pick_last') == 1
@@ -233,8 +235,7 @@ else % multiple recovery targets: allowed crash.
     end
 end
 
-%% results
-could_crash = crash;
-message = candidate_valid_messages{target_inst_index};
+%% Final result
+recovered_message = candidate_valid_messages{target_inst_index};
 
-display('Done!');
+display(['Done, recovered message is ' recovered_message]);
