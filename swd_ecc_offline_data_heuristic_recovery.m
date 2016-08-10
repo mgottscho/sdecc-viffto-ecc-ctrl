7e2897c6b69479df01f1e0f74f005517fc3af188
Mark Gottscho <mgottscho@ucla.edu>
2016-08-09 19:48:52 -0700

    Updated offline data heuristic recovery to not require reading the entire trace file which can be huge.

--- swd_ecc_offline_data_heuristic_recovery.m
+++ swd_ecc_offline_data_heuristic_recovery.m
@@ -51,29 +51,86 @@ if ~isdeployed
     addpath ecc common rv64g % Add sub-folders to MATLAB search paths for calling other functions we wrote
 end
 
-%% Set up parallel computing
-pctconfig('preservejobs', true);
-mypool = parpool(n_threads);
+%% Read data as hex-strings from file.
 
-%% Read data under test as bit-strings from file
+% Because the file may have a LOT of data, we don't want to read it into a buffer, as it may fail and use too much memory.
+% Instead, we get the number of instructions by using the 'wc' command, with the assumption that each line in the file will
+% contain a cache line.
 display('Reading inputs...');
+[wc_return_code, wc_output] = system(['wc -l ' input_filename]);
+if wc_return_code ~= 0
+    display(['FATAL! Could not get line count (# cache lines) from ' input_filename '.']);
+    return;
+end
+total_num_cachelines = str2num(strtok(wc_output));
+display(['Number of randomly-sampled words to test SWD-ECC: ' num2str(num_words) '. Total cache lines in trace: ' num2str(total_num_cachelines) '.']);
+
+%% Randomly choose cache lines from the trace, and load them
+rng('shuffle'); % Seed RNG based on current time
+sampled_cacheline_indices = sortrows(randperm(total_num_cachelines, num_words)'); % Randomly permute the indices of cachelines. We will choose the first num_words of the permuted list to evaluate. Then, from each of these cachelines, we randomly pick one word from within it.
+sampled_blockpos_indices = randi(words_per_block, 1, num_words); % Randomly generate the block position within the cacheline
+
 fid = fopen(input_filename);
-file_contents = textscan(fid, '%s', 'Delimiter', ',');
+if fid == -1
+    display(['FATAL! Could not open file ' input_filename '.']);
+    return;
+end
+
+% Loop over each line in the file and read it.
+% Only save data from the line if it matches one of our sampled indices.
+sampled_trace_raw = cell(num_words,1);
+j = 1;
+for i=1:total_num_cachelines
+    line = fgets(fid);
+    if strcmp(line,'') == 1 || j > size(sampled_cacheline_indices,1)
+        break;
+    end
+    if i == sampled_cacheline_indices(j)
+        sampled_trace_raw{j,1} = line;
+        j = j+1;
+    end
+end
 fclose(fid);
-file_contents = file_contents{1};
-file_contents = reshape(file_contents, (9+words_per_block), size(file_contents,1)/(9+words_per_block))';
-%trace_hex = textread(input_filename, '%8c');
-trace_paddr = char(file_contents(:,4));
-trace_blkpos = char(file_contents(:,9));
-trace_cachelines_hex = cell(size(file_contents,1),words_per_block);
-trace_cachelines_bin = cell(size(file_contents,1),words_per_block);
-total_num_cachelines = size(trace_cachelines_bin,1);
-for i=1:size(file_contents,1)
+
+%% Parse the raw trace
+% It is in CSV format, as output by our memdatatrace version of RISCV Spike simulator of the form
+% STEP,OPERATION,MEM_ACCESS_SEQ_NUM,VADDR,PADDR,USER_PERM,SUPER_PERM,ACCESS_SIZE,PAYLOAD,CACHE_BLOCKPOS,CACHE_BLOCK0,CACHE_BLOCK1,...,
+% like so:
+% 1805000,D$ RD fr MEM,1898719,VADDR 0x0000000000001718,PADDR 0x0000000000001718,u---,sRWX,4B,PAYLOAD 0x63900706,BLKPOS 3,0x33d424011374f41f,0x1314340033848700,0x0335040093771500,0x63900706638e0908,0xeff09ff21355c500,0x1315a50013651500,0x2330a4001355a500,0x1b0979ff9317c500,
+% ...
+% NOTE: memdatatrace payloads and cache blocks are in NATIVE byte order for
+% the simulated architecture. For RV64G this is LITTLE-ENDIAN!
+% NOTE: we only expect data cache lines to be in this file!
+% NOTE: addresses and decimal values in these traces are in BIG-ENDIAN
+% format.
+sampled_trace_step = cell(num_words,1);
+sampled_trace_operation = cell(num_words,1);
+sampled_trace_seq_num = cell(num_words,1);
+sampled_trace_vaddr = cell(num_words,1);
+sampled_trace_paddr = cell(num_words,1);
+sampled_trace_user_perm = cell(num_words,1);
+sampled_trace_supervisor_perm = cell(num_words,1);
+sampled_trace_payload_size = cell(num_words,1);
+sampled_trace_payload = cell(num_words,1);
+sampled_trace_demand_blockpos = cell(num_words,1);
+sampled_trace_cachelines_hex = cell(num_words,words_per_block);
+sampled_trace_cachelines_bin = cell(num_words,words_per_block);
+for i=1:num_words
+    remain = sampled_trace_raw{i,1};
+    [sampled_trace_step{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_operation{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_seq_num{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_vaddr{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_paddr{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_user_perm{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_supervisor_perm{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_payload_size{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_payload{i,1}, remain] = strtok(remain,',');
+    [sampled_trace_demand_blockpos{i,1}, remain] = strtok(remain,',');
     for j=1:words_per_block
-        trace_cachelines_hex{i,j} = char(file_contents(i,9+j));
-        tmp = trace_cachelines_hex{i,j};
-        trace_cachelines_hex{i,j} = tmp(1,3:end);
-        trace_cachelines_bin{i,j} = my_hex2bin(trace_cachelines_hex{i,j});
+        [block, remain] = strtok(remain,',');
+        sampled_trace_cachelines_hex{i,j} = block(3:end);
+        sampled_trace_cachelines_bin{i,j} = my_hex2bin(sampled_trace_cachelines_hex{i,j});
     end
 end
 
@@ -90,24 +147,19 @@ for i=1:n-1
     end
 end
 
-
-
-%% Randomly choose words from the trace, and do the fun parts on those
-rng('shuffle'); % Seed RNG based on current time
-sampled_cacheline_indices = randperm(total_num_cachelines, num_words); % Randomly permute the indices of cachelines. We will choose the first num_words of the permuted list to evaluate. Then, from each of these cachelines, we randomly pick one word from within it.
-sampled_blockpos_indices = randi(words_per_block, 1, num_words); % Randomly generate the block position within the cacheline
-sampled_trace_cachelines_hex = trace_cachelines_hex(sampled_cacheline_indices,:);
-sampled_trace_cachelines_bin = trace_cachelines_bin(sampled_cacheline_indices,:);
-
-display(['Number of randomly-sampled words to test SWD-ECC: ' num2str(num_words)]);
-display('Evaluating SWD-ECC...');
-
 results_candidate_messages = NaN(num_words,num_error_patterns); % Init
 success = NaN(num_words, num_error_patterns); % Init
 could_have_crashed = NaN(num_words, num_error_patterns); % Init
 success_with_crash_option = NaN(num_words, num_error_patterns); % Init
 verbose_recovery = '0';
 
+display('Evaluating SWD-ECC...');
+
+%% Set up parallel computing
+pctconfig('preservejobs', true);
+mypool = parpool(n_threads);
+
+%% Do the hard work
 parfor i=1:num_words % Parallelize loop across separate threads, since this could take a long time. Each word is a totally independent procedure to perform.
     %% Get the cacheline and "message," which is the original word, i.e., the ground truth from input file.
     cacheline_hex  = sampled_trace_cachelines_hex(i,:);
