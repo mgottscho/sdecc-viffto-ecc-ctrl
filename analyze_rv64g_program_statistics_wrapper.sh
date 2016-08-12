c4c187019ca7d2e142c79c213b2f01a553dc58ed
Mark Gottscho <mgottscho@ucla.edu>
2016-08-12 10:19:41 -0700

    Added a line to the program analysis wrapper script to also dump data cache lines into separate files from inst cache lines.

--- analyze_rv64g_program_statistics_wrapper.sh
+++ analyze_rv64g_program_statistics_wrapper.sh
@@ -7,6 +7,7 @@ INPUT_FILE=$4
 OUTPUT_FILE=$5
 
 ./extract_inst_lines_from_memdatatrace.sh $INPUT_FILE ${INPUT_FILE}.inst
+./extract_data_lines_from_memdatatrace.sh $INPUT_FILE ${INPUT_FILE}.data
 
 # Library paths are for running on Hoffman2
 LD_PRELOAD=$GCC5/lib64/libstdc++.so.6 LD_LIBRARY_PATH=$MATLAB/bin/glnxa64:$MATLAB/runtime/glnxa64:$LD_LIBRARY_PATH $BINARY_LOCATION/analyze_rv64g_program_statistics $BENCHMARK $K ${INPUT_FILE}.inst $OUTPUT_FILE
