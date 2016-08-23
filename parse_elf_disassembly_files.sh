9d533a0d07e55872870ff2a207641f7f42198409
Mark Gottscho <mgottscho@ucla.edu>
2016-08-22 19:52:53 -0700

    Added new bash script that parses through benchmark disassembly txt and correctly formats output files to be used by static offline recovery analysis.

---
+++ parse_elf_disassembly_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INPUT_PREFIX=$1
+FILE_1=$INPUT_PREFIX-disassembly.txt
+FILE_2=$INPUT_PREFIX-disassembly-formatted.txt
+FILE_3=$INPUT_PREFIX-instructions.txt
+
+echo "Input file: $FILE_1"
+
+sed -e "/Disassembly of section/d" $FILE_1 | sed -e "/^$/d" | sed -e "/>:/d" | sed -e "1d" | sed -r -e "s/\s*([0-9a-f]*:)/\1/g" | sed -r -e "s/([0-9a-f]{8})\s*/\1: /g" | sed -e "s/\t/ /g" > $FILE_2
+sed -r -e "s/[0-9a-f]*: ([0-9a-f]*): .*/\1/g" $FILE_2 > $FILE_3
