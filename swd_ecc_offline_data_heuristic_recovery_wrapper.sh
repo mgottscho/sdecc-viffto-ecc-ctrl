ebb5301735274e6e3fb2ddd6cd76c8e656f99a4d
Mark Gottscho <mgottscho@ucla.edu>
2016-09-26 10:56:57 -0700

    Trying to support DFM software environment, not yet working. Has library loading issues at runtime right now.

--- swd_ecc_offline_data_heuristic_recovery_wrapper.sh
+++ swd_ecc_offline_data_heuristic_recovery_wrapper.sh
@@ -19,6 +19,8 @@ if [[ "$MWG_MACHINE_NAME" == "hoffman" ]]; then
     MY_PRELOAD=$GCC5/lib64/libstdc++.so.6
 elif [[ "$MWG_MACHINE_NAME" == "nanocad-server-testbed" ]]; then
     MY_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+elif [[ "$MWG_MACHINE_NAME" == "dfm" ]]; then
+    MY_PRELOAD=/usr/lib64/libstdc++.so.6 # Not working as of 9/26/2016
 fi
 
 LD_PRELOAD=$MY_PRELOAD LD_LIBRARY_PATH=$MCRROOT/bin/glnxa64:$MCRROOT/runtime/glnxa64:$LD_LIBRARY_PATH $BINARY_LOCATION/swd_ecc_offline_data_heuristic_recovery $ISA $BENCHMARK $N $K $NUM_WORDS $NUM_SAMPLED_ERROR_PATTERNS $WORDS_PER_BLOCK $INPUT_FILE $OUTPUT_FILE $NUM_THREADS $CODE_TYPE $POLICY $VERBOSE_RECOVERY
--- swd_ecc_offline_inst_heuristic_recovery_wrapper.sh
+++ swd_ecc_offline_inst_heuristic_recovery_wrapper.sh
@@ -20,6 +20,8 @@ if [[ "$MWG_MACHINE_NAME" == "hoffman" ]]; then
     MY_PRELOAD=$GCC5/lib64/libstdc++.so.6
 elif [[ "$MWG_MACHINE_NAME" == "nanocad-server-testbed" ]]; then
     MY_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+elif [[ "$MWG_MACHINE_NAME" == "dfm" ]]; then
+    MY_PRELOAD=/usr/lib64/libstdc++.so.6 # Not working as of 9/26/2016
 fi
 
 LD_PRELOAD=$MY_PRELOAD LD_LIBRARY_PATH=$MCRROOT/bin/glnxa64:$MCRROOT/runtime/glnxa64:$LD_LIBRARY_PATH $BINARY_LOCATION/swd_ecc_offline_inst_heuristic_recovery $ISA $BENCHMARK $N $K $NUM_MESSAGES $NUM_SAMPLED_ERROR_PATTERNS $INPUT_FILE $OUTPUT_FILE $NUM_THREADS $CODE_TYPE $POLICY $MNEMONIC_HOTNESS_FILENAME $RD_HOTNESS_FILENAME $VERBOSE_RECOVERY
