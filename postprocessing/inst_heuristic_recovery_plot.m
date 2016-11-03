89feca8e851844827868539ed289a2d54d693493
Mark Gottscho <mgottscho@ucla.edu>
2016-11-02 23:05:14 -0700

    stuff

--- postprocessing/inst_heuristic_recovery_plot.m
+++ postprocessing/inst_heuristic_recovery_plot.m
@@ -125,26 +125,38 @@ close(gcf);
 
 if strcmp(code_type,'hsiao1970') == 1 || strcmp(code_type,'davydov1991') == 1
     z=1;
-    success_2d = NaN(n+1,n+1);
-    mean_success = mean(success);
+    cc_2d = NaN(n+1,n+1);
+    mean_results_candidate_messages = mean(success);
     for err_bitpos_1=1:n-1
         for err_bitpos_2=err_bitpos_1+1:n
-            success_2d(err_bitpos_1,err_bitpos_2) = mean_success(z);
+            cc_2d(err_bitpos_2,err_bitpos_1) = mean_success(z);
             z=z+1;
         end
     end
-    success_2d(end,:) = NaN;
-    success_2d(:,end) = NaN;
 
     figure;
-    pcolor(success_2d);
-    xlim([0 n]);
-    ylim([0 n]);
-
+    %mycolors = flipud(hot);
+    %colormap(mycolors);
+    pcolor(cc_2d);
     xlabel('Index of 1st bit in error', 'FontSize', 12, 'FontName', 'Arial');
     ylabel('Index of 2nd bit in error', 'FontSize', 12, 'FontName', 'Arial');
-    title(['Rate of Heuristic Recovery for ' code_type ' (' num2str(n) ',' num2str(k) ') -- ' benchmark ' -- ' policy], 'FontSize', 12, 'FontName', 'Arial');
+    set(gca,'XAxisLocation','top'); 
+    set(gca,'YDir','reverse');
 
+    set(gca,'XTick',[1 [5:5:n+1]]);
+    xtick = get(gca, 'XTick');
+    xticklabel = get(gca, 'XTickLabel');
+    set(gca,'XTick',xtick+0.6);
+    set(gca,'XTickLabel',xticklabel);
+
+    set(gca,'YTick',[2 [5:5:n]]);
+    %set(gca,'YTickLabel',fliplr([[1 5:5:n+1]]));
+    ytick = get(gca, 'YTick');
+    yticklabel = get(gca, 'YTickLabel');
+    set(gca,'YTick',[ytick(1:end-1)+0.6 ytick(end)]);
+    set(gca,'YTickLabel',yticklabel);
+    set(gca,'TickLength',[0 0]);
+    %title(['Number of Candidate Codewords for ' code_type ' (' num2str(n) ',' num2str(k) ')'], 'FontSize', 12, 'FontName', 'Arial');
     print(gcf, '-depsc2', [output_directory filesep architecture '-' benchmark '-inst-heuristic-recovery-heatmap.eps']);
     close(gcf);
 end
