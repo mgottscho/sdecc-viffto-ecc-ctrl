c7cbe2d75a137a72a5004969aecd58f57eb67018
Mark Gottscho <mgottscho@ucla.edu>
2017-03-09 23:06:49 -0800

    Reverted offline data recovery script parameters

--- automate_offline_data_recovery.sh
+++ automate_offline_data_recovery.sh
@@ -19,27 +19,27 @@ if [[ "$INPUT_TYPE" == "static" ]]; then # Static evaluation
     SPEC_BENCHMARKS="400.perlbench 401.bzip2 403.gcc 410.bwaves 416.gamess 429.mcf 433.milc 434.zeusmp 435.gromacs 436.cactusADM 437.leslie3d 444.namd 445.gobmk 447.dealII 450.soplex 453.povray 454.calculix 456.hmmer 458.sjeng 459.GemsFDTD 462.libquantum 464.h264ref 465.tonto 470.lbm 471.omnetpp 473.astar 481.wrf 482.sphinx3 483.xalancbmk" # Static -- all are working
     INPUT_DIRECTORY=$MWG_DATA_PATH/swd_ecc_data/$ISA/data-snapshots
 elif [[ "$INPUT_TYPE" == "dynamic" ]]; then # Dynamic
-    #SPEC_BENCHMARKS="400.perlbench 401.bzip2 403.gcc 410.bwaves 435.gromacs 436.cactusADM 444.namd 447.dealII 450.soplex 453.povray 454.calculix 456.hmmer 458.sjeng 459.GemsFDTD 462.libquantum 464.h264ref 465.tonto 470.lbm 471.omnetpp 473.astar" # Dynamic -- working
-    SPEC_BENCHMARKS="410.bwaves 435.gromacs 436.cactusADM 444.namd 447.dealII 450.soplex 453.povray 454.calculix 459.GemsFDTD 465.tonto 470.lbm" # Dynamic -- working floats
+    SPEC_BENCHMARKS="400.perlbench 401.bzip2 403.gcc 410.bwaves 435.gromacs 436.cactusADM 444.namd 447.dealII 450.soplex 453.povray 454.calculix 456.hmmer 458.sjeng 459.GemsFDTD 462.libquantum 464.h264ref 465.tonto 470.lbm 471.omnetpp 473.astar" # Dynamic -- working
+    #SPEC_BENCHMARKS="410.bwaves 435.gromacs 436.cactusADM 444.namd 447.dealII 450.soplex 453.povray 454.calculix 459.GemsFDTD 465.tonto 470.lbm" # Dynamic -- working floats
     INPUT_DIRECTORY=$MWG_DATA_PATH/swd_ecc_data/$ISA/spike_separated_float_int # For dynamic
 else
     echo "ERROR, bad INPUT_TYPE: $INPUT_TYPE"
     exit 1
 fi
 
-N=34
-K=32
+N=72
+K=64
 NUM_WORDS=1000
-WORDS_PER_BLOCK=16
+WORDS_PER_BLOCK=8
 NUM_THREADS=$(cat /proc/cpuinfo | grep ^processor | wc -l ) 
-CODE_TYPE=ULEL_float
+CODE_TYPE=hsiao1970
 NUM_SAMPLED_ERROR_PATTERNS=1000 # sampled
 #NUM_SAMPLED_ERROR_PATTERNS=741 # Max for (39,32) SECDED
 #NUM_SAMPLED_ERROR_PATTERNS=2556 # Max for (72,64) SECDED
 #NUM_SAMPLED_ERROR_PATTERNS=14190 # Max for (45,32) DECTED
 #NUM_SAMPLED_ERROR_PATTERNS=79079 # Max for (79,64) DECTED
 #NUM_SAMPLED_ERROR_PATTERNS=141750 # Max for (144,128) ChipKill
-POLICY=fdelta-pick-random
+POLICY=min-entropy8-pick-longest-run
 CRASH_THRESHOLD=0.5
 VERBOSE_RECOVERY=0
 
@@ -73,8 +73,8 @@ for SPEC_BENCHMARK in $SPEC_BENCHMARKS; do
         INPUT_FILE="$INPUT_DIRECTORY/${ISA}-${SPEC_BENCHMARK}-data.txt" # For static analysis
         # TODO
     elif [[ "$INPUT_TYPE" == "dynamic" ]]; then # Dynamic evaluation
-        #INPUT_FILE="$INPUT_DIRECTORY/spike_mem_data_trace_${SPEC_BENCHMARK}.txt.data.reads" # For dynamic analysis
-        INPUT_FILE="$INPUT_DIRECTORY/spike_mem_data_trace_${SPEC_BENCHMARK}.txt.data.reads.floats" # For dynamic analysis
+        INPUT_FILE="$INPUT_DIRECTORY/spike_mem_data_trace_${SPEC_BENCHMARK}.txt.data.reads" # For dynamic analysis
+        #INPUT_FILE="$INPUT_DIRECTORY/spike_mem_data_trace_${SPEC_BENCHMARK}.txt.data.reads.floats" # For dynamic analysis
     fi
     
     OUTPUT_FILE="$OUTPUT_DIRECTORY/${ISA}-${SPEC_BENCHMARK}-data-heuristic-recovery.mat"
