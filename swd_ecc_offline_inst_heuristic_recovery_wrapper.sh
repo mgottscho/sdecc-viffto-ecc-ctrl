de6d7329d17d414f6c7330249672d6b5c93bc86c
Mark Gottscho <mgottscho@ucla.edu>
2017-03-13 16:19:17 -0700

    Support for MWG-Desktop-UbuntuVM in helper scripts

--- swd_ecc_offline_inst_heuristic_recovery_wrapper.sh
+++ swd_ecc_offline_inst_heuristic_recovery_wrapper.sh
@@ -25,6 +25,8 @@ elif [[ "$MWG_MACHINE_NAME" == "nanocad-server-testbed" ]]; then
     MY_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
 elif [[ "$MWG_MACHINE_NAME" == "dfm" ]]; then
     MY_PRELOAD=/usr/lib64/libstdc++.so.6 # Not working as of 9/26/2016
+elif [[ "$MWG_MACHINE_NAME" == "mwg-desktop-ubuntuvm" ]]; then
+    MY_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
 fi
 
 LD_PRELOAD=$MY_PRELOAD LD_LIBRARY_PATH=$MCRROOT/bin/glnxa64:$MCRROOT/runtime/glnxa64:$LD_LIBRARY_PATH $BINARY_LOCATION/swd_ecc_offline_inst_heuristic_recovery $ISA $BENCHMARK $N $K $NUM_MESSAGES $NUM_SAMPLED_ERROR_PATTERNS $INPUT_FILE $OUTPUT_FILE $NUM_THREADS $CODE_TYPE $POLICY $MNEMONIC_HOTNESS_FILENAME $RD_HOTNESS_FILENAME $CRASH_THRESHOLD $VERBOSE_RECOVERY $FILE_VERSION $HASH_MODE
