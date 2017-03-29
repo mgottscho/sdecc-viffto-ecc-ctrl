bc1eceab583de6a917718e53d44d78f31dbcd8a1
Mark Gottscho <mgottscho@ucla.edu>
2017-03-28 19:48:05 -0700

    Changes to spike wrappers for SDELC and ESWEEK17

--- candidate_messages_spike_wrapper.sh
+++ candidate_messages_spike_wrapper.sh
@@ -5,9 +5,8 @@ ORIGINAL_MESSAGE_BIN=$1
 N=$2
 K=$3
 CODE_TYPE=$4
-POLICY=$5
 VERBOSE=0
-HASH_MODE=8
+HASH_MODE=none
 
 if [[ "$MWG_MACHINE_NAME" == "hoffman" ]]; then
     MY_PRELOAD=$GCC5/lib64/libstdc++.so.6
