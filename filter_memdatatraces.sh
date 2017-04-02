e4d983488874d57ed306226290d777a9503b53d0
Mark Gottscho <mgottscho@ucla.edu>
2017-04-02 14:54:39 -0700

    added filter support for faultlink benchmarks

--- filter_memdatatraces.sh
+++ filter_memdatatraces.sh
@@ -18,9 +18,10 @@ TARGET_DIR=$1
 #SPEC_BENCHMARKS="400.perlbench 401.bzip2 403.gcc 410.bwaves 416.gamess 429.mcf 433.milc 434.zeusmp 435.gromacs 436.cactusADM 437.leslie3d 444.namd 445.gobmk 447.dealII 450.soplex 453.povray 454.calculix 456.hmmer 458.sjeng 459.GemsFDTD 462.libquantum 464.h264ref 465.tonto 470.lbm 471.omnetpp 473.astar 481.wrf 482.sphinx3 483.xalancbmk 998.specrand 999.specrand" # Static
 SPEC_BENCHMARKS="400.perlbench 401.bzip2 403.gcc 410.bwaves 435.gromacs 436.cactusADM 444.namd 447.dealII 450.soplex 453.povray 454.calculix 456.hmmer 458.sjeng 459.GemsFDTD 462.libquantum 464.h264ref 465.tonto 470.lbm 471.omnetpp 473.astar" # Dynamic -- working
 AXBENCH_BENCHMARKS="fft blackscholes inversek2j jmeint jpeg sobel"
+FAULTLINK_BENCHMARKS="blowfish dhrystone matmult_int sha whetstone"
 ###############################################################################################
 
-BENCHMARKS=$AXBENCH_BENCHMARKS
+BENCHMARKS=$FAULTLINK_BENCHMARKS
 for BENCHMARK in $BENCHMARKS; do
 	echo "$BENCHMARK..."
     SOURCE_FILE="${TARGET_DIR}/spike_mem_data_trace_${BENCHMARK}.txt" 
