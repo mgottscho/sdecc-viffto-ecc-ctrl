db0f389db3705267c58f620884c8137ba5bf393d
Mark Gottscho <mgottscho@ucla.edu>
2017-04-04 20:20:39 -0700

    updates to ULEL recov plotter

--- postprocessing/ULEL_recovery_plotter.m
+++ postprocessing/ULEL_recovery_plotter.m
@@ -1,18 +1,35 @@
-bar([avg_benchmark_successes_data avg_benchmark_successes_inst]);
-colormap([0 0 0; 0.7 0.7 0.7]);
+concat_data = [
+     avg_benchmark_successes_data_r1(1:10)...
+     avg_benchmark_successes_data_r2(1:10)...
+     avg_benchmark_successes_data_r3(1:10);...
+     NaN NaN NaN;...
+     mean(avg_benchmark_successes_data_r1) mean(avg_benchmark_successes_data_r2) mean(avg_benchmark_successes_data_r3);...
+     avg_benchmark_successes_inst_r1(1:10)...
+     avg_benchmark_successes_inst_r2(1:10)...
+     avg_benchmark_successes_inst_r3(1:10);...
+     NaN NaN NaN;...
+     mean(avg_benchmark_successes_inst_r1) mean(avg_benchmark_successes_inst_r2) mean(avg_benchmark_successes_inst_r3);...
+     ];
+benchmark_names_repeat = benchmark_names;
+benchmark_names_repeat{12} = 'avg. data';
+benchmark_names_repeat(13:23) = benchmark_names;
+benchmark_names_repeat{24} = 'avg. inst.';
+bar(concat_data);
+colormap(flipud(gray(3)));
 set(gca,'xcolor','black','ycolor','black','fontsize',10,'fontname','arial');
 ylabel('Avg. Recov. Rate');
 %xlabel('Benchmark');
-set(gca,'xticklabel',benchmark_names, 'FontSize', 10);
+set(gca,'xtick',(1:24));
+set(gca,'xticklabel',benchmark_names_repeat, 'FontSize', 10);
 set(gca,'ytick',[0:0.2:1]);
 xticklabel_rotate([],45,[],'fontsize',10)
-pos = get(gca,'Position');
-pos(3) = 0.8;
-pos(4) = 0.5;
-set(gca,'Position',pos);
+%pos = get(gca,'Position');
+%pos(3) = 0.8;
+%pos(4) = 0.5;
+%set(gca,'Position',pos);
 grid on;
-lgd = legend({'Data Memory'; 'Instruction Memory'}, 'FontSize', 10);
-pos = get(lgd,'Position');
-pos(1) = 0.25;
-pos(2) = 0.635;
-set(lgd,'Position',pos);
+lgd = legend({'r=1', 'r=2', 'r=3'}, 'FontSize', 10);
+%pos = get(lgd,'Position');
+%pos(1) = 0.25;
+%pos(2) = 0.635;
+%set(lgd,'Position',pos);
